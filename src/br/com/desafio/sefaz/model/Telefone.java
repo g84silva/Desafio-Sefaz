5a1ad5a4283f491d59385a596f5f96b2b4c38b60
GSilva <gleibson-silva@hotmail.com>
2021-02-24 11:51:28 -0300

    Commit inicial, implementação das entidades model

---
+++ src/br/com/desafio/sefaz/model/Telefone.java
@@ -0,0 +1,76 @@
+package br.com.desafio.sefaz.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+public class Telefone {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private int ddd;
+	
+	@Column(nullable = false)
+	private String numero;
+	
+	@Column(nullable = false)
+	private String tipo;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	public Telefone() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(int ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "telefone => id: " + this.id + ", ddd: " + this.ddd + ", numero: " + this.numero + ", tipo: " + this.tipo;
+	}
+}
---
+++ src/br/com/desafio/sefaz/model/Usuario.java
@@ -0,0 +1,69 @@
+package br.com.desafio.sefaz.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "usuario_id")
+	private List<Telefone> telefones;
+	
+	public Usuario() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+	
+	@Override
+	public String toString() {
+		return "usuario => id: " + this.id + ", email: " + this.email + ", nome: " + this.nome + ", telefones: " + this.telefones.toString();
+	}
+}
